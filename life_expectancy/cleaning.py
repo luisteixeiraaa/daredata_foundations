7cb431f7f009377d1a46144801db9f096cac2819
luisteixeiraaa <brito961012@gmail.com>
2024-11-06 12:31:37 +0000

    update cleaning function

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -9,13 +9,17 @@ def clean_data(country) -> None:
 
     life_expectancy = pd.read_csv('./life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t')
     life_expectancy = pd.melt(life_expectancy, id_vars=life_expectancy.columns[0],
-                              value_vars=life_expectancy.columns[1:-1],
+                              value_vars=life_expectancy.columns[1:],
                               var_name="year")
+    
     heads = ['unit', 'sex', 'age', 'region']
     life_expectancy[heads] = life_expectancy.iloc[:, 0].str.split(r'[,\s]+', expand=True)
     life_expectancy.drop(columns=life_expectancy.columns[0], axis=1, inplace=True)
-    life_expectancy["year"] = pd.to_numeric(life_expectancy["year"]).astype(int)
-    life_expectancy["value"] = life_expectancy["value"].str.extract(r'(\d+.\d+)').astype('float')
+    life_expectancy["year"] = life_expectancy["year"].str.extract(r'(\d+)').astype(int)
+    life_expectancy["value"] = life_expectancy["value"].str.extract(r'(\d+.\d+)').astype(float)
+    life_expectancy = life_expectancy[life_expectancy["value"].notna()]
+    life_expectancy = life_expectancy[heads + ["year", "value"]]
+
     life_expectancy_pt = life_expectancy[life_expectancy["region"] == country]
     life_expectancy_pt.to_csv("./life_expectancy/data/pt_life_expectancy.csv", index=False)
 
