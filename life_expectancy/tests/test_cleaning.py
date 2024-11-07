263c7014bec09bfbc29230e63967516d47176da8
luisteixeiraaa <brito961012@gmail.com>
2024-11-07 16:01:31 +0000

    adapt test_cleaning

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,16 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import clean_data, load_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data("PT")
+    country_life_expectancy = load_data()
+    country_life_expectancy = clean_data("PT", country_life_expectancy)
+    save_data("PT", country_life_expectancy)
+
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
